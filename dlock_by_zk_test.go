29ba7606de55049e1aebb72e40b820bcbf42ff34
zhoujian2 <zhoujian2@sensetime.com>
2020-12-21 14:17:59 +0800

    fix test case TestDLockByZookeeper

--- dlock_by_zk_test.go
+++ dlock_by_zk_test.go
@@ -13,18 +13,11 @@ var (
 	fakeZKEndpoints = []string{"127.0.0.1:2181", "127.0.0.1:2182", "127.0.0.1:2183"}
 )
 
-func TestFastDLocker(t *testing.T) {
+func TestDLockByZookeeper(t *testing.T) {
 	conn, _ := EstablishZKConn(fakeZKEndpoints)
 	defer CloseZKConn(conn)
 
 	total := 0
-	successCh := make(chan int, 20)
-	go func() {
-		for success := range successCh {
-			total += success
-		}
-		assert.Equal(t, 20, total)
-	}()
 
 	var n sync.WaitGroup
 	for i := 0; i < 20; i++ {
@@ -36,13 +29,11 @@ func TestFastDLocker(t *testing.T) {
 			if dl.TryLock(5) {
 				time.Sleep(time.Microsecond * 100)
 				dl.Unlock()
-				successCh <- 1
-			} else {
-				successCh <- -1
+				total += 1
 			}
 		}(conn, i)
 	}
 	n.Wait()
 
-	close(successCh)
+	assert.Equal(t, 20, total)
 }
